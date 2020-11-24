24fa741ad6b32c7eb568b5fa7018cc19fe229ab7
Adi <aramachandran@olin.edu>
2020-11-23 18:15:49 -0800

    graph stuff

--- control_mousebot/scripts/graphbuilder.py
+++ control_mousebot/scripts/graphbuilder.py
@@ -1,25 +1,40 @@
 """
-class to scan Lidar scan to build graph understanding 
+class to scan Lidar scan to build graph maze understanding 
+Theory - building should only interact with four nearest walls 
 """
+
+class Node(object): 
+    def __init__(self, x,y, left=None, right=None, front=None, back=None): 
+        self.left = left
+        self.right = right
+        self.front = front
+        self.back = back
+        self.x = x
+        self.y = y
+
+    def update(self): 
+        pass 
 
 class Graph(object): 
     def __init__(self, dimensions=(64,64)): 
         self.create_nodes(dimensions)
     
     def create_nodes(self, dimensions): 
-        pass
+        for x in range(dimensions[0]): 
+            for y in range(dimensions[1]): 
+                 
 
 
 class GraphBuilder(object):
     def __init__(self, range=5): 
         # rospy shit 
-        self.Graph = Graph()
+        self.graph = Graph()
         self.range = range
         self.centerpoints = {
-            'left': 90
-            'right': 270
-            'front': 0
-            'back': 180
+            'left': 90,
+            'right': 270,
+            'front': 0,
+            'back': 180,
         }
 
     def add_to_graph(self, results): 
@@ -27,6 +42,11 @@ class GraphBuilder(object):
 
     def scan_range(self, range, centerpoint): 
         pass
+
+    def check_nodes(self): 
+        """
+        Check all adjacent nodes in graph
+        """
     
     def check_surroundings(self): 
         results = self.scan_range()
