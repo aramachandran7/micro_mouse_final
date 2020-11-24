784d88e3aa5b5343b04da8b8b61ace62f8064a14
Adi <aramachandran@olin.edu>
2020-11-23 19:06:38 -0800

    graph stuffgit commit -m

--- control_mousebot/scripts/graphbuilder.py
+++ control_mousebot/scripts/graphbuilder.py
@@ -3,27 +3,24 @@ class to scan Lidar scan to build graph maze understanding
 Theory - building should only interact with four nearest walls 
 """
 
-class Node(object): 
-    def __init__(self, x,y, left=None, right=None, front=None, back=None): 
-        self.left = left
-        self.right = right
-        self.front = front
-        self.back = back
-        self.x = x
-        self.y = y
-
-    def update(self): 
-        pass 
-
 class Graph(object): 
-    def __init__(self, dimensions=(64,64)): 
-        self.create_nodes(dimensions)
+    def __init__(self, side_length=(16)): 
+        self.create_nodes(side_length)
+        self.graph = []
     
-    def create_nodes(self, dimensions): 
-        for x in range(dimensions[0]): 
-            for y in range(dimensions[1]): 
-                 
-
+    def create_nodes(self, side_length): 
+        for x in range(side_length+1): 
+            # there are 17 main lists within the big list, the evens are 16 long the odds are 17 long 
+            # the evens are vertical, the odds are horizontal walls. 
+            self.graph.append([])
+            if x % 2 == 0: 
+                for i in range(16): 
+                    self.graph[x].append(False)
+            else: 
+                for i in range(17): 
+                    self.graph[x].append(False)
+                
+                
 
 class GraphBuilder(object):
     def __init__(self, range=5): 
@@ -37,18 +34,20 @@ class GraphBuilder(object):
             'back': 180,
         }
 
-    def add_to_graph(self, results): 
-        pass 
 
     def scan_range(self, range, centerpoint): 
         pass
 
-    def check_nodes(self): 
-        """
-        Check all adjacent nodes in graph
-        """
     
-    def check_surroundings(self): 
+    def check_surroundings(self, nc): 
+        surrounding_nodes = self.return_surrounding_nodes(nc)
         results = self.scan_range()
         self.add_to_graph(results)
+
+    def return_surrounding_nodes(self, nc): 
+        """
+        neato coordinates tuple of (0,0)
+        """
+        surrounding_nodes = {"left":(nc[0], nc[1]), "right":(nc[0]+2, nc[1]), "front":(nc[0]+1, nc[1]+1),"back": (nc[0]+1,nc[1])}
+        return surrounding_nodes
 
