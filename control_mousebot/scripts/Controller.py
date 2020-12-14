813fe7ec919429f4201f881464800645f13fad0e
Adi <aramachandran@olin.edu>
2020-12-14 00:46:28 -0800

    restructure controller class

--- control_mousebot/scripts/Controller.py
+++ control_mousebot/scripts/Controller.py
@@ -9,90 +9,95 @@ import rospy
 import time
 import numpy as np
 
+class Controller(object):
+    def __init__(self):
 
+        # params for driving
+        self.speed = 0.3
+        unit_length = 0.193
 
-def run():
-    # setting constants
-    speed = 0.3
-    unit_length = 0.193
-    # pos = (0,0)
-    pos = (0,0)
-    target_list = [(7, 7), (7, 8), (8, 7), (8, 8)]
-    len_nodes_visited = 5
-    nodes_visited = [0]*len_nodes_visited
-    pointer = 0
+        # positions
+        self.pos = (0,0)
+        self.target_list = [(7, 7), (7, 8), (8, 7), (8, 8)]
 
-    # creating objects
-    MoveComputer = MoveComputer2()
-    graph = Graph2()
-    driver = DriveStep(pos, unit_length=unit_length)
-    planner = PathPlanner()
+        # circular buffer for nodes visited
+        self.len_nodes_visited = 5
+        self.nodes_visited = [0]*len_nodes_visited
+        self.pointer = 0
 
-    # time.sleep(3) # hold up
-    walls = driver.return_walls(first=True) # compute first walls before movement
-    print('walls returned first: ', walls)
+        # initializing objects
+        self.MoveComputer = MoveComputer2()
+        self.graph = Graph2()
+        self.driver = DriveStep(pos, unit_length=unit_length)
+        self.planner = PathPlanner()
 
-    # test pathplanning code
-    target = (8,7)
-    previous_graph = np.load('mazes/graph.npy',allow_pickle='TRUE').item()
-    path_to_center = planner.a_star(previous_graph, start=pos, target=target)
-    for position in path_to_center:
-        print('movement!=======================================================================')
-        print("Moving to:   ", position)
-        walls = driver.drive(position, speed) # updates walls, position
-        pos = position
-        print(' ')
-        time.sleep(.3)
+        self.previous_graph = None # dictionary, not graph object.
 
-    print("At Center.")
+    def run(self):
+        """ blocking code while loop for mousebot reach center """
+        walls = self.driver.return_walls(first=True) # compute first walls before movement
+        print('walls returned first: ', walls)
+        pos = self.pos
+        while (pos not in target_list) and not rospy.is_shutdown():
+            print('movement!=======================================================================')
+            self.graph.update_graph(pos, walls)
+            next_pos = self.MoveComputer.compute_next_move(self.graph, pos)
+            print("Moving to:   ", next_pos)
+            walls = self.driver.drive(next_pos, speed) # updates walls, position
+            pos = next_pos
 
+            self.nodes_visited[pointer] = pos
 
+            counter = 0
+            for visited in self.nodes_visited:
+                if visited == self.nodes_visited[self.pointer]:
+                    counter += 1
+            if counter >=3:
+                print("you're in a back and forth loop, altering MoveComputer2 code")
+            # else:
+            #     MoveComputer.coef = 1.0
 
+            self.pointer = (self.pointer+1)%self.len_nodes_visited
+            print(' ')
+            time.sleep(.3)
 
+        print("reached center")
+        # save dictionary
+        np.save('mazes/graph.npy', self.graph.graph)
 
+    def test_pathplanning(self):
+        """test pathplanning code with imported maze graph"""
 
-    # blocking code while loop for mousebot reach center
-    # while (pos not in target_list) and not rospy.is_shutdown():
-    #     print('movement!=======================================================================')
-    #     graph.update_graph(pos, walls)
-    #     next_pos = MoveComputer.compute_next_move(graph, pos)
-    #     print("Moving to:   ", next_pos)
-    #     walls = driver.drive(next_pos, speed) # updates walls, position
-    #     pos = next_pos
-    #
-    #     nodes_visited[pointer] = pos
-    #
-    #     counter = 0
-    #     for visited in nodes_visited:
-    #         if visited == nodes_visited[pointer]:
-    #             counter += 1
-    #     if counter >=3:
-    #         print("you're in a back and forth loop, altering MoveComputer2 code")
-    #     # else:
-    #     #     MoveComputer.coef = 1.0
-    #
-    #     pointer = (pointer+1)%len_nodes_visited
-    #     print(' ')
-    #     time.sleep(.3)
-    #
-    # print("reached center")
-    # # save dictionary
-    # np.save('mazes/graph.npy', graph.graph)
-    #
-    # # code for mousebot to reverse track back to starting point
-    # target = (0,0)
-    # path_to_home = planner.a_star(graph.graph, start=pos, target=target)
-    #
-    # for position in path_to_home:
-    #     print('movement!=======================================================================')
-    #     print("Moving to:   ", position)
-    #     walls = driver.drive(position, speed) # updates walls, position
-    #     pos = position
-    #     print(' ')
-    #     time.sleep(.3)
-    #
-    # print("Back home.")
+        target = (8,7)
+        self.previous_graph = np.load('mazes/graph.npy',allow_pickle='TRUE').item()
+
+        path_to_center = self.planner.a_star(previous_graph, start=pos, target=target)
+        for position in path_to_center:
+            print('movement!=======================================================================')
+            print("Moving to:   ", position)
+            walls = self.driver.drive(position, speed) # updates walls, position
+            pos = position
+            print(' ')
+            time.sleep(.3)
+
+        print("At Center.")
+
+    def get_home(self):
+        # code for mousebot to reverse track back to starting point
+        target = (0,0)
+        path_to_home = self.planner.a_star(self.graph.graph, start=self.pos, target=target)
+        pos = self.pos
+        for position in path_to_home:
+            print('movement!=======================================================================')
+            print("Moving to:   ", position)
+            walls = self.driver.drive(position, speed) # updates walls, position
+            pos = position
+            print(' ')
+            time.sleep(.3)
+
+        print("Back home.")
 
 
 if __name__ == '__main__':
-    run()
+    control = Controller()
+    control.run()
